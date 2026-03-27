46cd0a0129ae9031e7ba8fc6f1ffab355fc48a06
Prof. Juliana <133364388+Juliana-senaisp@users.noreply.github.com>
2026-03-27 13:05:30 -0300

    .

---
+++ Projetos/PPT/script.js
@@ -0,0 +1,21 @@
+//JS
+function jogar(escolhaUsuario) {
+    let opcoes = ["pedra", "papel", "tesoura"]
+                //   0        1         2
+    let escolhaComputador = opcoes[Math.floor(Math.random()*3)]
+
+    let final = ""
+    if(escolhaUsuario == escolhaComputador) {
+        final = "Empate"
+    } else if (
+        (escolhaUsuario == "pedra" && escolhaComputador == "tesoura") ||
+        (escolhaUsuario == "papel" && escolhaComputador == "pedra") ||
+        (escolhaUsuario == "tesoura" && escolhaComputador == "papel")
+    ) {
+        final = "Jogador Ganhou!"
+    } else {
+        final = "Computador Ganhou!"
+    }
+    let resultado = document.getElementById("resultado")
+    resultado.innerText = "Jogador: "+escolhaUsuario + " | Computador: " + escolhaComputador + " = " + final
+}
